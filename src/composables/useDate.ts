22789131812ef197ec7b50c0f6f8055244c70679
AlenaPetrova <alena_p99@mail.ru>
2025-07-09 10:16:08 +0300

    feat(useDate.ts): added methods for automatic calculation of dates of previous and current periods

--- src/composables/useDate.ts
+++ src/composables/useDate.ts
@@ -14,3 +14,30 @@ export const getYesterdayStr = (): string => {
   const dd = String(yesterday.getDate()).padStart(2, "0");
   return `${yyyy}-${mm}-${dd}`;
 };
+
+export const getWeekAgoStr = (): string => {
+  const weekAgo = new Date();
+  weekAgo.setDate(weekAgo.getDate() - 7);
+  const yyyy = weekAgo.getFullYear();
+  const mm = String(weekAgo.getMonth() + 1).padStart(2, "0");
+  const dd = String(weekAgo.getDate()).padStart(2, "0");
+  return `${yyyy}-${mm}-${dd}`;
+};
+
+export const getTwoWeeksAgoStr = (): string => {
+  const twoWeeksAgo = new Date();
+  twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
+  const yyyy = twoWeeksAgo.getFullYear();
+  const mm = String(twoWeeksAgo.getMonth() + 1).padStart(2, "0");
+  const dd = String(twoWeeksAgo.getDate()).padStart(2, "0");
+  return `${yyyy}-${mm}-${dd}`;
+};
+
+export const getEightDaysAgoStr = (): string => {
+  const eightDaysAgo = new Date();
+  eightDaysAgo.setDate(eightDaysAgo.getDate() - 8);
+  const yyyy = eightDaysAgo.getFullYear();
+  const mm = String(eightDaysAgo.getMonth() + 1).padStart(2, "0");
+  const dd = String(eightDaysAgo.getDate()).padStart(2, "0");
+  return `${yyyy}-${mm}-${dd}`;
+};
