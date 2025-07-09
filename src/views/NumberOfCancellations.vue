d66df30746073110cc04ab01ba2446dc15b1b323
AlenaPetrova <alena_p99@mail.ru>
2025-07-09 11:36:17 +0300

    feat(NumberOfCancellations.vue): a page for the is_cancel(true) of goods metric has been developed, displaying a graph and table of all articles in periods

--- src/views/NumberOfCancellations.vue
+++ src/views/NumberOfCancellations.vue
@@ -1,9 +1,76 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useOrderStore } from "@/stores/orderStore";
+import { storeToRefs } from "pinia";
+import Table from "@/components/Table.vue";
+import ChartMetrics from "@/components/ChartMetrics.vue";
+
+const orderStore = useOrderStore();
+const {
+  cancelMetrics,
+  sumCancelMetrics,
+  loadingOrders,
+  loadingAllOrders,
+  error,
+} = storeToRefs(orderStore);
+const { fetchOrders, fetchAllOrders } = orderStore;
+
+onMounted(async () => {
+  fetchOrders(1);
+  fetchAllOrders();
+});
+
+const tableColumnsName = [
+  { key: "nm_id", label: "Артикул" },
+  { key: "prev", label: "Предыдущая неделя" },
+  { key: "current", label: "Текущая неделя" },
+  { key: "change", label: "Изменение" },
+];
+const routingColumnName = "nm_id";
+const to = { name: "article" };
+
+const chartColumnsame = ["Предыдущая неделя", "Текущая неделя"];
+</script>
 
 <template>
   <div class="container">
-    <h1>Кол-во отмен заказов</h1>
+    <h1>Количество отмен заказов</h1>
+
+    <div class="wrapper">
+      <h2 v-if="loadingAllOrders || !Object.keys(sumCancelMetrics).length">
+        Загрузка данных для графика...
+      </h2>
+      <h2 v-else-if="error">{{ error }}</h2>
+      <ChartMetrics
+        v-else
+        :obj="sumCancelMetrics"
+        :periods="chartColumnsame"
+        title=""
+        title-x="Период"
+        title-y="Кол-во отмен"
+      />
+
+      <h2 v-if="loadingOrders || !cancelMetrics.length">
+        Загрузка данных для таблицы...
+      </h2>
+      <h2 v-else-if="error">{{ error }}</h2>
+      <Table
+        v-else
+        :columns="tableColumnsName"
+        :rows="cancelMetrics"
+        clickable
+        :to="to"
+        :column-name-with-id="routingColumnName"
+        show-svg
+      />
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
